6be7de4360c4f00be5906ecde2369b265be61540
getzze <getzze@gmail.com>
2014-02-25 13:08:32 +0000

    List subtitles using imdb_id

--- subliminal/providers/opensubtitles.py
+++ subliminal/providers/opensubtitles.py
@@ -127,9 +127,13 @@ class OpenSubtitlesProvider(Provider):
         query = None
         if ('opensubtitles' not in video.hashes or not video.size) and not video.imdb_id:
             query = video.name.split(os.sep)[-1]
-        return self.query(languages, hash=video.hashes.get('opensubtitles'), size=video.size, imdb_id=video.imdb_id,
+        subs = self.query(languages, hash=video.hashes.get('opensubtitles'), size=video.size, imdb_id=video.imdb_id,
                           query=query)
-
+        if video.imdb_id is not None:
+            subs.extend(self.query(languages, hash=None, size=video.size, imdb_id=video.imdb_id,
+                              query=query))
+        return subs
+        
     def download_subtitle(self, subtitle):
         response = checked(self.server.DownloadSubtitles(self.token, [subtitle.id]))
         if not response['data']:
