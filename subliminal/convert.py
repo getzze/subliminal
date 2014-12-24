f66767452ee1d264bdf78cdf637899f637675d0c
getzze <getzze@gmail.com>
2014-12-25 00:02:42 +0100

    Correct bug broken symlink
    Symlink are overwritten when broken. Videos converted to mkv even if no subtitles were found. Video converter stdout and stderr redirected to logger.

--- subliminal/convert.py
+++ subliminal/convert.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from __future__ import unicode_literals, print_function
 import subprocess
 import os
+import traceback
+import difflib
 import logging
 import babelfish
 import enzyme
 #from .subtitle import get_subtitle_path
 from subliminal.subtitle import get_subtitle_path
+from subliminal.video import Video, Movie, Episode
 #from subliminal.video import scan_subtitle_languages
+
+#: COUNTRY LONG NAMES
+LONG_COUNTRIES = {v:k for k, v in babelfish.COUNTRIES.iteritems()}
 
 logger = logging.getLogger(__name__)
 
@@ -58,10 +64,13 @@ def scan_available_subtitle_languages(path, sub_extensions='.srt'):
     :rtype: set
 
     """
-    language_extensions = tuple('.' + c for c in babelfish.get_language_converter('alpha2').codes)
+    language_extensions = tuple('.' + c for c in babelfish.language_converters['alpha2'].codes)
     dirpath, filename = os.path.split(path)
     subtitles = set()
-    for p in os.listdir(dirpath):
+    for pp in os.listdir(dirpath):
+        p = pp
+        if isinstance(pp, bytes):
+           p = pp.decode('utf8')
         if p.startswith(os.path.splitext(filename)[0]) and p.endswith(sub_extensions):
             if os.path.splitext(p)[0].endswith(language_extensions):
                 subtitles.add(babelfish.Language.fromalpha2(os.path.splitext(p)[0][-2:]))
@@ -70,8 +79,77 @@ def scan_available_subtitle_languages(path, sub_extensions='.srt'):
     logger.debug('Found subtitles %r', subtitles)
     return subtitles
                 
-                
-def convert_videos(videos, languages=None, language=babelfish.Language('eng'), subtitles_format='.srt', video_format='.mkv', video_savepath=None, single=True, delete_subtitles=False, force_copy=False, force_mkvmerge=False):
+def sort_video(video, savepath):
+    """Try to sort the video according to its type and name
+
+    :param video: video to sort
+    :param savepath: root path where the file will be stored
+    :return extrapath: additional arborescence.
+    """
+    if isinstance(video, Episode):
+        try:
+            video.imdb_update()
+        except AttributeError:
+            logger.debug('Update from imdbID not implemented.')
+        series_path = os.path.join(savepath, 'Series')
+        series_name=""
+        if os.path.exists(series_path):        
+            series_name_list=difflib.get_close_matches(video.series, os.walk(os.path.join(savepath, "Series")).next()[1], 1, cutoff=0.8)
+            if len(series_name_list)>0:
+                series_name=series_name_list[0]
+        if len(series_name)==0:
+            series_name = video.series
+        arborescence = os.path.join("Series", series_name, "Season %d" % video.season)
+    elif isinstance(video, Movie):
+        arborescence = "Films"
+    else:
+        logger.info("Video must be Movie or Episode") 
+        arborescence = "Others"
+    return os.path.join(savepath, arborescence)
+
+def simlink_movie(video, savepath, real_path_video, sort_by=['country','genre']):
+    """Create a symbolic link of the video to the country and arborescence
+
+    :param video: video to sort
+    :param savepath: root path for the video arborescence
+    :param real_path_video: real path of the video that the softlink will direct to.
+    :param list sort_by: List of parameters to make an arborescence with. For ex., 'country', will create the arborescence savepath/By country/<video country>/softlink 
+    """
+    if not os.path.isfile(real_path_video):
+        logger.info("Original video does not exist: '%s'" % real_path_video)
+        return
+
+    available_sorting = {'lang':'Langue', 'country':'Pays', 'genre':'Genre'}
+    if isinstance(video, Episode):
+        # Only for Movie type of vidoes
+        return
+    if not video.imdb_id:
+        video.getimdb()
+    for sort in sort_by:
+        if sort not in available_sorting:
+            return
+
+        if not video.__dict__.get(sort, None) and video.imdb_id:
+            video.imdb_update()
+        if video.__dict__.get(sort, None):
+            Kinds = video.__dict__.get(sort, '').split(', ')
+            for kind in Kinds: 
+                arborescence = os.path.join("Films", "Par %s" %(available_sorting[sort].title()), kind.title())
+                link_path = os.path.join(savepath, arborescence)
+                # Create symbolic link directory
+                if not os.path.isdir(link_path):
+                    os.makedirs(link_path)
+                dst = os.path.join(link_path, os.path.basename(real_path_video))
+                if os.path.islink(dst):
+                    # Remove symlink if it already exists
+                    os.remove(dst)                    
+                    logger.info("Destination link already exists, overwritten: '%s'" % dst) 
+                # Create symbolic link
+                os.symlink(os.path.realpath(real_path_video), dst)
+                logger.info("Symbolic link created at: '%s'" % dst) 
+    return  
+
+def convert_videos(videos, languages=None, language=babelfish.Language('eng'), subtitles_format='.srt', video_format='.mkv', video_savepath=None, single=True, delete_subtitles=False, force_copy=False, force_mkvmerge=False, create_soft_link=True):
     """Convert `videos` in .mkv using ffmpeg, including subtitles with the existing languages if not specified by `languages`.
 
     :param videos: videos to convert in .mkv
@@ -115,14 +193,30 @@ def convert_videos(videos, languages=None, language=babelfish.Language('eng'), s
             sformat = '.' + subtitles_format.strip('.')
             outformat = '.' + video_format.strip('.')
             #lang = language.alpha3
-            
+
             # define path for the output video and check if the output video already exists
             if video_savepath:
-                outpath = sort_video(video, video_savepath)
+                output_video_path = sort_video(video, video_savepath)
             else:
-                outpath = dirpath
-            outvid_apath = os.path.join(outpath, Name + outformat)
+                output_video_path = dirpath
+            
+#            # define path for the output video and check if the output video already exists
+#            if video_savepath:
+#                outpath = video_savepath
+#            else:
+#                outpath = dirpath
+#            extrapath=""
+#            if full_sorting:
+#                extrapath=sort_video(video, outpath)
+            if not video.title is None:
+                try:
+                    Name = "%02d - %s" % (video.episode, video.title)
+                except AttributeError:
+                    Name = video.title
+#            outvid_apath = os.path.join(outpath, extrapath, Name + outformat)
+            outvid_apath = os.path.join(output_video_path, Name + video_format)
             if os.path.isfile(outvid_apath) and (not force_copy):
+#            if os.path.isfile(output_video) and (not force_copy):
                 # check if .mkv contains the subtitles for these languages   
                 embedded_subtitles = scan_embedded_subtitle_languages(outvid_apath)
                 logger.info('Embedded subtitles : %r', embedded_subtitles)
@@ -159,9 +253,15 @@ def convert_videos(videos, languages=None, language=babelfish.Language('eng'), s
                     command = ["ffmpeg", "-loglevel", "panic"] + command
                 else:
                     command = ["ffmpeg"] + command
-                
-                Proc = subprocess.Popen(command)
+
+		logger.debug("Launching : '%s'", " ".join(command))
+                Proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = Proc.communicate()
                 logger.info('Converting video %s ------', filename)
+                if stdout:
+                    logger.info(stdout)
+                if stderr:
+                    logger.error(stderr)
                 Proc.wait()
                 logger.info('---- Conversion finished.')
                 converted_videos.add(path)
@@ -176,7 +276,7 @@ def convert_videos(videos, languages=None, language=babelfish.Language('eng'), s
                         logger.info("Error code: %r", e.code) 
             
             # for multiple subtitles, mkvmerge is used
-            elif (force_mkvmerge and len(embeddable_subtitles)>0) or (not single and len(embeddable_subtitles)>=2):
+            elif (force_mkvmerge and len(embeddable_subtitles)>=0) or (not single and len(embeddable_subtitles)>=2):
                 command = []
                 if loglevel >= 30:
                     command = ['-q']
@@ -198,9 +298,14 @@ def convert_videos(videos, languages=None, language=babelfish.Language('eng'), s
                     # define command to mkvmerge to embed the subtitle with `lang` language
                     command += ['--language', '0:%s'%(lang.alpha3), insub_apath] 
 
-                    
-                Proc = subprocess.Popen(command)
+                logger.debug("Launching : '%s'", " ".join(command))
+                Proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdout, stderr = Proc.communicate()
                 logger.info('Converting video %s ------', filename)
+                if stdout:
+                    logger.info(stdout)
+                if stderr:
+                    logger.error(stderr)
                 Proc.wait()
                 logger.info('---- Conversion finished.')
                 converted_videos.add(path)
@@ -222,32 +327,16 @@ def convert_videos(videos, languages=None, language=babelfish.Language('eng'), s
                             logger.info("Error code: %r", e.code) 
             else:
                 logger.debug('No subtitles to embed')
+
+            # Create second arborescence for Movies with symbolic links
+            if video_savepath is not None and create_soft_link:
+                try:
+                    simlink_movie(video, video_savepath, outvid_apath)
+                except OSError as e:
+                    logger.info("Symlink creation failed with: {} {}".format(e.code, e.strerror))
+
         except Exception as e: # name the Exception `e`
-            logger.exception("Failed to convert video %r", path) 
+            logger.exception("Failed to convert video %r : %r", path, e) 
             # continue to next video if error occured in one video
             continue 
     return converted_videos
-
-def sort_video(video, outpath):
-    """Try to sort the video according to its type and name
-
-    :param video: video to sort
-    :param outpath: root path where the file will be stored
-    :return extrapath: additional arborescence.
-    """
-    if isinstance(video, Episode):
-        try:
-            video.imdb_update()
-        except AttributeError:
-            logger.debug('Update from imdbID not implemented.')
-        series_path = os.path.join(outpath, 'Series')
-        series_name=difflib.get_close_matches(video.series,  [ f for f in os.listdir(series_path) if isdir(join(series_path,f)) ], 1, cutoff=0.9)
-        if series_name:
-            series_name = video.series
-        arborescence = os.path.join("Series", series_name, "Season %d" % video.season)
-    elif isinstance(video, Movie):
-        arborescence = "Films"
-    else:
-        logger.info("Video must be Movie or Episode") 
-        arborescence = "Others"  
-    return os.path.join(outpath, arborescence)
